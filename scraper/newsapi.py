caccfd7e724b5883212d3e87bd49aa8ba3603d7c
Race Peterson <race.peterson@gmail.com>
2024-05-24 00:29:24 -0600

    ig

--- scraper/newsapi.py
+++ scraper/newsapi.py
@@ -6,27 +6,24 @@ with open("config.yaml", "r") as f:
     config = yaml.safe_load(f)
 
 # Replace 'your_api_key' with your actual API key
-url = f'https://newsapi.org/v2/top-headlines?sources=reuters,bbc-news,al-jazeera,cnn&apiKey={config["newsapi_key"]}'
+sources = 'the-hindu,techradar,rt,reuters,rbc,ynet,xinhua-net,argaam,la-nacion,globo'
+query = 'economy OR finance OR business'
+url = f'https://newsapi.org/v2/top-headlines?sources={sources}&apiKey={config["newsapi_key"]}' # in the last 10 mins or 10 hours??
 
 response = requests.get(url)
 data = response.json()
 
-for article in data['articles']:
-    print(f"Source: {article['source']['name']}")
-    print(f"Title: {article['title']}")
-    print(f"Description: {article['description']}\n")
+# Print the headlines and descriptions of the articles
+if data['status'] == 'ok':
+    for article in data['articles']:
+        print(f"Source: {article['source']['name']}")
+        print(f"Title: {article['title']}")
+        print(f"Description: {article['description']}\n")
+else:
+    print(f"Failed to retrieve news: {data['message']}")
 
 
 '''
 sources:
-    the-hindu
-    techradar
-    rt
-    reuters
-    rbc
-    ynet
-    xinhua-net
-    argaam
-    la-nacion
-    globo
+    the-hindu,techradar,rt,reuters,rbc,ynet,xinhua-net,argaam,la-nacion,globo
 '''
